545e56e23524f9af6c1b38d68dc57936d602a977
sorin-bolos <sorinalbolos@yahoo.com>
2025-08-29 01:03:46 +0300

    Remove copilot_installExtension tool

--- src/extension/tools/node/allTools.ts
+++ src/extension/tools/node/allTools.ts
@@ -15,7 +15,6 @@ import './getNotebookCellOutputTool';
 import './getSearchViewResultsTool';
 import './githubRepoTool';
 import './insertEditTool';
-import './installExtensionTool';
 import './newNotebookTool';
 import './notebookSummaryTool';
 import './readFileTool';
